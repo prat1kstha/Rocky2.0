68dacac48c0f8d0d1dc93ef4cd8d8308db3b28b7
pratik.shrestha <pratik.shrestha@cotiviti.com>
2022-01-05 21:51:47 +0545

    Update Order Details

--- Rocky_Main/Controllers/OrderController.cs
+++ Rocky_Main/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Braintree;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models;
 using Rocky_Models.ViewModels;
 using Rocky_Utility;
 using Rocky_Utility.BrainTree;
@@ -67,6 +69,70 @@ namespace Rocky.Controllers
             };
 
             return View(OrderVM);
+        }
+
+        [HttpPost]
+        public IActionResult StartProcessing()
+        {
+            OrderHeader orderHeader = _orderHeaderRepo.Find(OrderVM.OrderHeader.Id);
+            orderHeader.OrderStatus = Constants.StatusProcessing;
+            _orderHeaderRepo.Save();
+            TempData[Constants.Success] = "Order Is In Process!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult ShipOrder()
+        {
+            OrderHeader orderHeader = _orderHeaderRepo.Find(OrderVM.OrderHeader.Id);
+            orderHeader.OrderStatus = Constants.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            _orderHeaderRepo.Save();
+            TempData[Constants.Success] = "Order Shipped Successfully!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult CancelOrder()
+        {
+            OrderHeader orderHeader = _orderHeaderRepo.Find(OrderVM.OrderHeader.Id);
+
+            var gateway = _brain.GetGateway();
+            Transaction transaction = gateway.Transaction.Find(orderHeader.TransactionId);
+
+            if (transaction.Status == TransactionStatus.AUTHORIZED || transaction.Status == TransactionStatus.SUBMITTED_FOR_SETTLEMENT) 
+            {
+                //no refund
+                Result<Transaction> resultVoid = gateway.Transaction.Void(orderHeader.TransactionId);
+            }
+            else
+            {
+                //refund
+                Result<Transaction> resultRefund = gateway.Transaction.Refund(orderHeader.TransactionId);
+            }
+
+            orderHeader.OrderStatus = Constants.StatusRefunded;
+            _orderHeaderRepo.Save();
+            TempData[Constants.Success] = "Order Cancelled Successfully!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateOrderDetails()
+        {
+            OrderHeader orderHeaderFromDb = _orderHeaderRepo.Find(OrderVM.OrderHeader.Id);
+            orderHeaderFromDb.FullName = OrderVM.OrderHeader.FullName;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+            orderHeaderFromDb.Email = OrderVM.OrderHeader.Email;
+
+            _orderHeaderRepo.Save();
+            TempData[Constants.Success] = "Order Details Updated Successfully!";
+
+            return RedirectToAction("Details", "Order", new { id = orderHeaderFromDb.Id });
         }
     }
 }
